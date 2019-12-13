36f2dc38a035ac2fd199d89eef8c8359ca322d8d
Ben Bancroft <ben@tutorful.co.uk>
2019-12-13 09:53:08 +0000

    build chat app

---
+++ index.js
@@ -0,0 +1,64 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+let numUsers = 0;
+
+io.on('connection', (socket) => {
+    let addedUser = false;
+    console.log('A user connected');
+
+    socket.on('new user', username => {
+        if (addedUser) return;
+
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    socket.on('chat message', message => {
+        socket.broadcast.emit('chat message', {
+            message: message,
+            username: socket.username
+        });
+    });
+
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('stop typing', () => {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('disconnect', () => {
+        if (addedUser) {
+            --numUsers;
+
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    })
+});
+
+http.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
